9d8bfb1739d2bf90258386508ae320a7cc7a5102
yliess86 <hatiyliess86@gmail.com>
2021-09-17 19:07:09 +0200

    [nerf] Moved fourier ferature mapping responsability to model

--- nerf/model.py
+++ nerf/model.py
@@ -1,5 +1,6 @@
 import torch
 
+from nerf.features import FourierFeatures
 from torch import Tensor
 from torch.nn import Linear, Module, ReLU, Sequential, Sigmoid
 from typing import Tuple
@@ -32,6 +33,9 @@ class NeRF(Module):
         fc_1 = [(self.phi_x_dim, self.width)] + fc
         fc_2 = [(self.phi_x_dim + self.width, self.width)] + fc
 
+        self.phi_x = FourierFeatures(3, self.phi_x_dim)
+        self.phi_d = FourierFeatures(3, self.phi_d_dim)
+
         self.fc_1 = Sequential(*(Sequential(Linear(*io), ReLU()) for io in fc_1))
         self.fc_2 = Sequential(*(Sequential(Linear(*io), ReLU()) for io in fc_2))
         
@@ -43,17 +47,20 @@ class NeRF(Module):
             Linear(self.width // 2, 3), Sigmoid(),
         )
 
-    def forward(self, phi_x: Tensor, phi_d: Tensor) -> Tuple[Tensor, Tensor]:
+    def forward(self, rx: Tensor, rd: Tensor) -> Tuple[Tensor, Tensor]:
         """Query NeRF
 
         Arguments:
-            phi_x (Tensor): ray position embedding (B, PHI_X_FEATURES)
-            phi_d (Tensor): ray direction embedding (B, PHI_D_FEATURES)
+            rx (Tensor): ray query position (B, 3)
+            rd (Tensor): ray query direction (B, 3)
 
         Returns:
             sigma (Tensor): volume density at query position (B, )
             rgb (Tensor): color at query position (B, 3)
         """
+        phi_x = self.phi_x(rx)
+        phi_d = self.phi_d(rd)
+
         x = self.fc_1(phi_x)
         x = self.fc_2(torch.cat((phi_x, x), dim=-1))
 
