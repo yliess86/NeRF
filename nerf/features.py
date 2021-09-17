05f710d024c8738275cbceebb0fe6f3707ad49aa
yliess86 <hatiyliess86@gmail.com>
2021-09-17 16:13:59 +0200

    [nerf] features Docstring

--- nerf/features.py
+++ nerf/features.py
@@ -7,16 +7,44 @@ from torch.nn import Module, Parameter
 
 
 def sample_b(size: Size, sigma: float) -> Tensor:
+    """Sample b matrix for fourier features
+    
+    Arguments:
+        size (Size): b matrix size
+        sigma (float): std of the gaussian
+
+    Returns:
+        b (Tensor): b matrix
+    """
     return torch.randn(size) * sigma
 
 
 @jit.scipt
 def map_fourier_features(v: Tensor, b: Tensor) -> Tensor:
+    """Map v to fourier features representation phi(v)
+    
+    Arguments:
+        v (Tensor): input features (B, IFeatures)
+        b (Tensor): b matrix (OFeatures, IFeatures)
+
+    Returns:
+        phi(v) (Tensor): fourrier features (B, 2 * OFeatures)
+    """
     a = 2 * PI * v @ b.T
     return torch.cat((torch.sin(a), torch.cos(a)), dim=-1)
 
 
 class FourierFeatures(Module):
+    """Fourier Features module
+
+    Maps v to fourier features representation phi(v)
+
+    Arguments:
+        i_dim (int): input dimension for v
+        features (int): output dimension (default: 256)
+        sigma (float): std of the gaussian (default: 6.)
+    """
+
     def __init__(
         self,
         i_dim: int,
@@ -32,4 +60,12 @@ class FourierFeatures(Module):
         self.b = Parameter(self.b, requires_grad=False)
 
     def forward(self, v: Tensor) -> Tensor:
+        """Map v to fourier features representation phi(v)
+    
+        Arguments:
+            v (Tensor): input features (B, IFeatures)
+
+        Returns:
+            phi(v) (Tensor): fourrier features (B, 2 * OFeatures)
+        """
         return map_fourier_features(v, self.b)
