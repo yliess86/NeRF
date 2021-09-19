f68f35e0298b604c16bf7b98a6d32e956f1fd7f0
yliess86 <hatiyliess86@gmail.com>
2021-09-19 15:47:49 +0200

    [Test] test training script

---
+++ test.py
@@ -0,0 +1,75 @@
+if __name__ == "__main__":
+    import nerf.infer
+    import nerf.train
+    import numpy as np
+    import torch
+    torch.backends.cudnn.benchmark = True
+
+    from PIL import Image    
+    from nerf.data import BlenderDataset
+    from nerf.core import NeRF
+    from nerf.core import BoundedVolumeRaymarcher as BVR
+    from torch.nn import MSELoss
+    from torch.optim import AdamW
+
+    
+    ROOT = "./data/blender"
+    SCENE = "hotdog"
+    STEP = 5
+
+    MODEL = f"./res/NeRF_{SCENE}.pt"
+    GT = f"./res/NeRF_{SCENE}_gt.png"
+    PRED = f"./res/NeRF_{SCENE}_pred.png"
+
+    FEATURES = 256
+    SIGMA = 26.
+
+    WIDTH = 256
+    DEPTH = 4
+    
+    TN, TF = 2., 6.
+    SAMPLES = 128
+
+    LR = 1e-3
+    WEIGHT_DECAY = 0
+
+    BATCH_SIZE = 1024
+    JOBS = 24
+
+    EPOCHS = 100
+    LOG = 50
+
+    train = BlenderDataset(ROOT, SCENE, "train", step=STEP)
+    val = BlenderDataset(ROOT, SCENE, "val", step=STEP)
+    test = BlenderDataset(ROOT, SCENE, "test", step=STEP)
+
+    W, H = test.W, test.H
+    C, ro, rd = test.C[:W * H], test.ro[:W * H], test.rd[:W * H]
+
+    gt = C.view(W, H, 3).clip(0, 1) * 255
+    gt = gt.numpy().astype(np.uint8)
+    Image.fromarray(gt).save(GT)
+
+    nerf = NeRF(FEATURES, FEATURES, SIGMA, SIGMA, width=WIDTH, depth=DEPTH).cuda()
+    raymarcher = BVR(TN, TF, samples=SAMPLES)
+    
+    criterion = MSELoss().cuda()
+    optim = AdamW(nerf.parameters(), lr=LR, weight_decay=WEIGHT_DECAY)
+
+    fit_args = raymarcher, optim, criterion, train, val, test
+    inf_args = raymarcher, ro, rd, W, H
+
+    for i in range(LOG):
+        history = nerf.fit(
+            *fit_args,
+            epochs=EPOCHS // LOG,
+            batch_size=BATCH_SIZE,
+            jobs=JOBS,
+        )
+
+        torch.save(nerf.state_dict(), MODEL)
+        nerf.load_state_dict(torch.load(MODEL))
+
+        pred = nerf.infer(*inf_args, batch_size=BATCH_SIZE)
+        pred = pred.numpy().astype(np.uint8)
+        Image.fromarray(pred).save(PRED)
