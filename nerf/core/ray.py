[nerf] Fixed Mid Point Computation for Ray PDF Sampling

nerf/core/ray.py
@@ -212,11 +212,11 @@ def pdf_rays(
     Nf = samples
     N = Nc + Nf
 
-    b = .5 * (t[:, :-1] - t[:, 1:])
+    b = .5 * (t[:, 1:] + t[:, :-1])
     w = weights[:, 1:-1]
 
-    z = pdf_z_values(b, w, Nf, ro.device, perturb)
-    t, _ = torch.sort(torch.cat((t, z), dim=-1), dim=-1)
+    t_pdf = pdf_z_values(b, w, Nf, ro.device, perturb)
+    t, _ = torch.sort(torch.cat((t, t_pdf), dim=-1), dim=-1)
     delta = segment_lengths(t, rd)
 
     rx = ro[:, None, :] + rd[:, None, :] * t[:, :, None]

nerf/core/renderer.py
@@ -46,7 +46,7 @@ def raymarch_volume(
     EPS = 1e-10
     
     alpha = 1 - torch.exp(-sigma * delta)
-    w = alpha * exclusive_cumprod(1 - alpha + EPS)
+    w = alpha * exclusive_cumprod(1. - alpha + EPS)
     return w, torch.sum(w[:, :, None] * rgb, dim=-2)
 
 
@@ -132,7 +132,7 @@ def render_volume_fine(
     rx = rx.view(B * N, 3)
     rds = rds.view(B * N, 3)
 
-    if train: nerf.requires_grad(train)
+    if train: nerf.requires_grad(True)
 
     sigma, rgb = nerf(rx, rds)
     sigma = sigma.view(B, N)