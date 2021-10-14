a646b7efb6333bb5c9b1de47c7755a69ed748bc7
yliess86 <hatiyliess86@gmail.com>
2021-10-14 14:20:15 +0200

    [test] Updated Tests w/ Indications

--- test.py
+++ test.py
@@ -2,7 +2,8 @@ import torch
 import torch.jit as jit
 
 from io import BytesIO
-from nerf.core import NeRF, BoundedVolumeRaymarcher as BVR
+from nerf.core import BoundedVolumeRaymarcher as BVR, NeRF
+from nerf.core import FourierFeatures as FF, PositionalEncoding as PE
 from torch.cuda.amp import autocast, GradScaler
 from torch.optim import SGD
 from torch.nn import MSELoss
@@ -10,37 +11,47 @@ from torch.nn import MSELoss
 
 DEVICE = "cuda:0"
 
-nerf = NeRF(16, 16, 26, 26, 32, 2).to(DEVICE)
-raymarcher = BVR(2., 6., 16, 16)
+n_fms = [FF, PE]
+x_fms = [FF(3, 16, 26), PE(3, 6)]
+d_fms = [FF(3, 16, 26), PE(3, 6)]
 
-cirterion = MSELoss().to(DEVICE)
-optim = SGD(nerf.parameters(), lr=5e-2, momentum=.9)
-scaler = GradScaler()
+for name, phi_x, phi_d in zip(n_fms, x_fms, d_fms):
+    print(f"[Test({name.__name__})] Init")
+    nerf = NeRF(phi_x, phi_d, 32, 2).to(DEVICE)
+    raymarcher = BVR(2., 6., 16, 16)
 
-nerf = nerf.train()
-for _ in range(2):
-    ro = torch.zeros((4, 3), requires_grad=False, device=DEVICE)
-    rd = torch.zeros((4, 3), requires_grad=False, device=DEVICE)
-    C = torch.zeros((4, 3), requires_grad=False, device=DEVICE)
+    cirterion = MSELoss().to(DEVICE)
+    optim = SGD(nerf.parameters(), lr=5e-2, momentum=.9)
+    scaler = GradScaler()
 
-    with autocast():
-        C_ = raymarcher.render_volume(nerf, ro, rd, perturb=True, train=True)
-        loss = cirterion(C_, C)
+    print(f"[Test({name.__name__})] Train")
+    nerf = nerf.train()
+    for _ in range(2):
+        ro = torch.ones((4, 3), requires_grad=False, device=DEVICE)
+        rd = torch.ones((4, 3), requires_grad=False, device=DEVICE)
+        C = torch.ones((4, 3), requires_grad=False, device=DEVICE)
 
-    scaler.scale(loss).backward()
-    scaler.step(optim)
-    scaler.update()
-    optim.zero_grad(set_to_none=True)
+        with autocast():
+            _, C_ = raymarcher.render_volume(nerf, ro, rd, perturb=True, train=True)
+            loss = cirterion(C_, C)
 
-buffer = BytesIO()
-jit.save(jit.script(nerf), buffer)
+        scaler.scale(loss).backward()
+        scaler.step(optim)
+        scaler.update()
+        optim.zero_grad(set_to_none=True)
 
-buffer.seek(0)
-nerf = jit.load(buffer).to(DEVICE)
+    print(f"[Test({name.__name__})] Save")
+    buffer = BytesIO()
+    jit.save(jit.script(nerf), buffer)
 
-ro = torch.zeros((4, 3), requires_grad=False, device=DEVICE)
-rd = torch.zeros((4, 3), requires_grad=False, device=DEVICE)
+    print(f"[Test({name.__name__})] Load")
+    buffer.seek(0)
+    nerf = jit.load(buffer).to(DEVICE)
 
-nerf = nerf.eval()
-with torch.inference_mode():
-    C_ = raymarcher.render_volume(nerf, ro, rd, perturb=True, train=False)
+    print(f"[Test({name.__name__})] Inference")
+    ro = torch.ones((4, 3), requires_grad=False, device=DEVICE)
+    rd = torch.ones((4, 3), requires_grad=False, device=DEVICE)
+
+    nerf = nerf.eval()
+    with torch.inference_mode():
+        C_ = raymarcher.render_volume(nerf, ro, rd, perturb=True, train=False)
